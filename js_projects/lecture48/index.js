9519b236d4abeae32758390e2171355874137541
tarun0001g <tarun0003g@gmail.com>
2025-09-20 16:27:14 +0530

    Add practice file for lecture 48

---
+++ js_projects/lecture48/index.js
@@ -0,0 +1,34 @@
+
+//-----IN-BUILT MATH OBJECT-----//
+
+console.log("Tarun");
+// console.log(Math.PI);
+// console.log(Math.round(1.11));
+// console.log(Math.round(1.6));
+// console.log(Math.max(2,5,3,88,669,98,686,1,2,3));
+// console.log(Math.min(2,5,3,88,669,98,686,-1,2,3));
+// console.log(Math.floor(1.99)); // returns just smaller INT
+// console.log(Math.ceil(1.1)); // returns just greater INT
+// console.log(Math.abs(-5)); //retuns positive
+// console.log(Math.random()); //returns any random number between 0-1
+// console.log(Math.sqrt(9));
+// console.log(Math.pow(2,10));
+
+
+//-----DATE OBJECT-------//
+
+// let curr = new Date();
+
+// console.log(curr);
+
+let date = new Date('Nov 6 2005 6:00');
+
+// date as a parameter
+let newDate = new Date(2005,10,6,6);
+//note that month and week numbering starts from 0-11, 0-6 (sunday to sat)
+
+console.log(newDate);
+console.log(newDate.getDay());
+// use online mdn and other sites for better understanding
+
+
