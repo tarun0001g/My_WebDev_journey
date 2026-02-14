82cfa654f92f0906cfeecfc7e9e8c432a1749f70
tarun0001g <tarun0003g@gmail.com>
2026-02-14 14:07:56 +0530

    Created first node.js server

--- Node js/Lec-2_Intro/first.js
+++ Node js/Lec-2_Intro/first.js
@@ -1,2 +1,13 @@
 
 console.log("Tarun Makavana is here!")
+
+const fs = require('fs');
+
+fs.writeFile('Text.txt', 'This is the text file created by me', (err) => {
+  if(err){
+    console.log("Error is occured!");
+  }
+  else{
+    console.log("File created successfully!");
+  }
+});
---
+++ Node js/Lec-3_FirstNodeServer/firstNode.js
@@ -0,0 +1,28 @@
+//SIMPLE NODE JS SERVER
+//we are in the server 
+
+
+const http = require('http');
+
+//The clint(browser) will send request to this server 
+// function requestListener(req, res){ //any fn will take same this 2 arguments (request, response)
+//   console.log(req);
+// }
+// http.createServer(requestListener);
+
+//latest short technique to create server
+ const server = http.createServer( (req, res)=>{ 
+  console.log(req);
+});
+
+//we should make keep our server in the listening mode to listen the requests from the client(browser)
+
+// server.listen(5000);
+//now when we run node firstNode.js in terminal, the server will be started and in the litening mode and we can see the request object in the terminal when we send request from the browser by typing localhost:5000 in the address bar.
+
+const PORT = 3000 ;
+server.listen(PORT, () => {
+  console.log('The server is running at http://localhost:' + PORT); 
+  //console.log('The server is running at http://localhost:${PORT}')
+})
+
