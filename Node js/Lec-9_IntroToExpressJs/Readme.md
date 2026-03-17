387975c465dafe1040fae430e14a4acb56345296
tarun0001g <tarun0003g@gmail.com>
2026-03-17 17:08:49 +0530

    Understand basics of express.js

--- Node js/Lec-9_IntroToExpressJs/Readme.md
+++ Node js/Lec-9_IntroToExpressJs/Readme.md
@@ -1,17 +1,36 @@
 
 
+
+
+
+# How to install express.js :-
+ npm install express --save
+
+ # How to use express ? 
+ //first add module in file
+ const express = require("express); //(External Module)
+
+//then, execute express FN and store it in a app(variable);
+
 # Middleware is a function that runs between request and response.
+- multiple middlewares can be added between them.
+ex. (req, res, next) => {...}
+- next() is an extra argument or method used to call next middleware 
+- The order of middleware is also very impotnat.
 
+# Handling routes in express.js :-
+- Routing means deciding how the server responds to a client request for a specific URL and HTTP method.
+- Route = URL + Function that runs when that URL is requested
 
+Rules:- 
+  Order Matters
+  Cannot call next() after  send() or end() (response)
+  "/" matches everything that startss with "/"
+  calling res.send() imlpcitly calls res.end()
 
-
-
-
-
-
-
-
-
+- app.use() is wildcard and matches to all http methods(get,post,put,delete,etc...) - its fn  executed everytime
+- app.get(), app.post() are executed only when there is a specific get/post requests are defined
+- if methods are clearly defined then the browser will strictly check url and method to execute fn(middleware)
 
 
 
--- Node js/Lec-9_IntroToExpressJs/app.js
+++ Node js/Lec-9_IntroToExpressJs/app.js
@@ -1,33 +1,89 @@
-//Core modue added
-// const http = require("http"); //we dont need when we use express, because express has predefined
-
-//External module added
-const express = require("express");
-//Local module added
-// const requestHandler = require('./user');
-const app = express(); //app is also kind of request handler, it 
-
-//Adding middleware
-app.use((req, res, next)=>{ //.use() is used to add middleware
-  console.log("In first middleware!", req.url, req.method);
-  next(); //it will move to next middleware
-})
-
-app.use((req, res, next)=>{
-  console.log("In second middleware!", req.url, req.method);
-  //Sending response in middleware
-  res.send("<h2>Nice to meet you sir!</h2>")
-})
 
 
-const PORT = 3005;
+//NORMAL SERVER CREATION USING NODEJS (WITHOUT EXPRESS):- 
 
-app.listen(PORT, () => {
-  console.log(`The server is running at http://localhost:${PORT}`);
-});
-
-// const server = http.createServer(app); // we also don't need this
+// const http = require("http"); //Core module added
+// const requestHandler = require('./user'); //Local module added
+// const server = http.createServer(requestHandler); 
 
 // server.listen(PORT, () => {
 //   console.log(`The server is running at http://localhost:${PORT}`);
 // });
+
+//BASIC SERVER CREATION USING EXPRESS.JS:-
+
+// const http = require("http"); ////Core modue:- we dont need when we use express, because express has predefined
+
+// const express = require("express"); //External module added
+
+// const app = express(); //app is also kind of request handler (like requestHandler) who takes req & res as params 
+
+// const server = http.createServer(app); 
+
+// const PORT = 3005;
+// server.listen(PORT, () => {
+//   console.log(`The server is running at http://localhost:${PORT}`);
+// });
+
+
+
+// REDUCED SERVER CREATION USING EXPRESS:-
+
+// const express = require("express");
+// const app = express();
+
+// //Adding middleware 
+// app.use((req, res, next)=>{ //.use() is used to add middleware
+//   console.log("In first middleware!", req.url, req.method);
+//   next(); //it will move to next middleware
+// })
+3
+// app.use((req, res, next)=>{
+//   console.log("In second middleware!", req.url, req.method);
+//   //Sending response in middleware
+//   res.send("<h2>Nice to meet you sir!</h2>");
+// })
+
+// const PORT = 3005;
+// app.listen(PORT, () => {
+//   console.log(`The server is running at http://localhost:${PORT}`);
+// });
+
+//HANDLING ROUTES IN EXPRESS.js:-
+const express = require("express");
+const app = express();
+
+//Note:- if methods are clearly defined then the browser will strictly check url and method to execute fn
+app.use("/", (req,res,next)=> { //it will not strictly check the url & method
+  console.log("In the first middleware!", req.url, req.method); 
+  //res.send("<h1>From first middleware!</h1>"); //cannot send next after send()
+  next();
+})
+
+app.get("/", (req,res,next)=> { //fn will exuted only when the url is "/" & GEt method, 
+  console.log("In the another middleware!", req.url, req.method); 
+  res.send("<h1>From another middleware!</h1>");
+  // next();
+})
+
+app.post("/submit-details", (req,res,next)=> { // Now browser will striclty check url and post method to execute fn
+  console.log("In the submit details middleware!", req.url, req.method); 
+  res.send("<h1>Details Submitted Successfully!</h1>");
+  // next();
+}) 
+
+const PORT = 3005;
+app.listen(PORT, () => {
+  console.log(`The server is running at http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
